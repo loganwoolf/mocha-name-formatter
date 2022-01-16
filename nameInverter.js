618eef2504d5d254224e2eeeb0d168c9571505fb
Logan Woolf <loganwoolf@gmail.com>
2022-01-16 08:12:44 +0000

    Implement nameInverter function
    Yet to implement throw error on undefined

--- nameInverter.js
+++ nameInverter.js
@@ -1,7 +1,23 @@
 const nameInverter = function(name) {
+  let honRegex = new RegExp(/[a-z]+\.$/i);
+  let manySpaces = new RegExp(/ {2,}/g);
+  name.replace(manySpaces, ' ');
+  name = name.trim();
   if (name === '') return '';
+  
+  name = name.split(' ');
+  let honorificPosition = name.findIndex(x => honRegex.test(x));
+  let honorific = name[honorificPosition];
+  if (honorificPosition >= 0) {
+    name.splice(honorificPosition, 1);
+  }
+  if (!name.length) return '';
+  name.reverse();
+  name = name.join(', ');
 
-  return name;
+  return typeof honorific === 'string' && honorific.length > 1
+    ? honorific + ' ' + name
+    : name;
 };
 
 module.exports = nameInverter;
